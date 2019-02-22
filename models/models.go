a49387d3d380e1d3bc020fcad03b40fd0ff98897
Jackey <muzi2960@aliyun.com>
2019-02-22 11:57:10 +0800

    分类管理

--- models/models.go
+++ models/models.go
@@ -5,41 +5,42 @@ import (
 	"github.com/unknwon/com"
 	"os"
 	"path"
+	"strconv"
 	"time"
 	_ "github.com/mattn/go-sqlite3"
 )
 
 const (
-	_DB_NAME = "data/beeblog.db"
+	_DB_NAME       = "data/beeblog.db"
 	_SQLITE_DRIVER = "sqlite3"
 )
 
 type Category struct {
-	Id int64
-	Title string
-	Created time.Time `orm:"index"`
-	Views int64 `orm:"index"`
-	TopicTime time.Time `orm:"index"`
-	TopicCount int64
+	Id              int64
+	Title           string
+	Created         time.Time `orm:"index"`
+	Views           int64     `orm:"index"`
+	TopicTime       time.Time `orm:"index"`
+	TopicCount      int64
 	TopicLastUserId int64
 }
 
 type Topic struct {
-	Id int64
-	Uid int64
-	Title string
-	Content string `orm:"size(5000)"`
-	Attachment string
-	Created time.Time `orm:"index"`
-	Update time.Time `orm:"index"`
-	Views int64 `orm:"index"`
-	Author string
-	ReplyTime time.Time `orm:"index"`
-	ReplyCount int64
+	Id              int64
+	Uid             int64
+	Title           string
+	Content         string `orm:"size(5000)"`
+	Attachment      string
+	Created         time.Time `orm:"index"`
+	Update          time.Time `orm:"index"`
+	Views           int64     `orm:"index"`
+	Author          string
+	ReplyTime       time.Time `orm:"index"`
+	ReplyCount      int64
 	ReplyLastUserId int
 }
 
-func RegisterDB()  {
+func RegisterDB() {
 	if !com.IsExist(_DB_NAME) {
 		os.MkdirAll(path.Dir(_DB_NAME), os.ModePerm)
 		os.Create(_DB_NAME)
@@ -47,4 +48,42 @@ func RegisterDB()  {
 	orm.RegisterModel(new(Category), new(Topic))
 	orm.RegisterDriver(_SQLITE_DRIVER, orm.DRSqlite)
 	orm.RegisterDataBase("default", _SQLITE_DRIVER, _DB_NAME, 10)
+}
+
+func AddCategory(name string) error {
+	o := orm.NewOrm()
+	cate := &Category{
+		Title: name,
+		Created: time.Now(),
+		TopicTime: time.Now(),
+	}
+	qs := o.QueryTable("category")
+	err := qs.Filter("title", name).One(cate)
+	if err == nil {
+		return err
+	}
+	_, err = o.Insert(cate)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func GetAllCategories() ([]*Category, error) {
+	o := orm.NewOrm()
+	cates := make([]*Category, 0)
+	qs := o.QueryTable("category")
+	_, err := qs.All(&cates)
+	return cates, err
+}
+
+func DelCategory(id string) error {
+	cid, err := strconv.ParseInt(id, 10, 64)
+	if err != nil {
+		return err
+	}
+	o := orm.NewOrm()
+	cate := &Category{Id: cid}
+	_, err = o.Delete(cate)
+	return err
 }
